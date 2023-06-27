be8c3a66835a58fed0fa4d33859afefa8e8b10fc
mathieu <mathieu.perin.prog@gmail.com>
2023-06-27 11:27:08 +0200

    mise en place du rooting

--- Projet/LeMentorat/src/app/app-routing.module.ts
+++ Projet/LeMentorat/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AboutUsComponent} from "./about-us/about-us.component";
+import {IndexComponent} from "./index/index.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'about-us', component: AboutUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule
+{
+}
--- Projet/LeMentorat/src/app/app.module.ts
+++ Projet/LeMentorat/src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NgOptimizedImage } from '@angular/common';
-import { PresentationComponent } from './index/presentation/presentation.component';
-import { LastNewsComponent } from './index/last-news/last-news.component';
-import { MentorsComponent } from './index/mentors/mentors.component';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { WitnessesComponent } from './index/witnesses/witnesses.component';
-import { RedirectContactComponent } from './index/redirect-contact/redirect-contact.component';
-import { FooterComponent } from './footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {NgOptimizedImage} from '@angular/common';
+import {PresentationComponent} from './index/presentation/presentation.component';
+import {LastNewsComponent} from './index/last-news/last-news.component';
+import {MentorsComponent} from './index/mentors/mentors.component';
+import {SlickCarouselModule} from 'ngx-slick-carousel';
+import {WitnessesComponent} from './index/witnesses/witnesses.component';
+import {RedirectContactComponent} from './index/redirect-contact/redirect-contact.component';
+import {FooterComponent} from './footer/footer.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MethodDropdownComponent} from './navbar/dropdown/method-dropdown/method-dropdown.component';
+import {FreeContentDropdownComponent} from './navbar/dropdown/free-content-dropdown/free-content-dropdown.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +28,10 @@ import { HttpClientModule } from '@angular/common/http';
     WitnessesComponent,
     RedirectContactComponent,
     FooterComponent,
+    MethodDropdownComponent,
+    FreeContentDropdownComponent,
+    AboutUsComponent,
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,4 +43,6 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule
+{
+}
