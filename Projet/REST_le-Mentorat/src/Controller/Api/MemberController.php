709ae79b844f680083b868c7aa365d4c811c8d67
mathieu <mathieu.perin.prog@gmail.com>
2023-06-29 12:02:23 +0200

    api recup staff
    mise ne place de la recuperation des membres du staff via l'api

--- Projet/REST_le-Mentorat/src/Controller/Api/MemberController.php
+++ Projet/REST_le-Mentorat/src/Controller/Api/MemberController.php
@@ -8,11 +8,49 @@ use App\Repository\RoleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/members')]
 class MemberController extends AbstractController
 {
+
+#[Route("/staff", methods: ['GET'])]
+    public function getStaff(MemberRepository $memberRepository, RoleRepository $jobRepository, UserPasswordHasherInterface $hasher, EntityManagerInterface $entityManager):JsonResponse{
+
+        $create = new Member();
+        $create->setFirstName('rayan')
+            ->setLastName('ryn')
+            ->setJob($jobRepository->find(6))
+            ->setEmail('rayan@mentorat.com')
+            ->setDescription('Commercial')
+            ->setPseudo('closer2')
+            ->setAvatar('rayan.jpg')
+            ->setPassword($hasher->hashPassword($create, 1234))
+            ->setRoles(['ROLE_USER']);
+        $entityManager->persist($create);
+        $entityManager->flush();
+
+        $staff = $memberRepository->findAll();
+        $data = [];
+    foreach ($staff as $member) {
+        $data[] = [
+            'lastname' => $member->getLastName(),
+            'description' => $member->getDescription(),
+            'avatar' => $member->getAvatar(),
+            'pseudo' => $member->getPseudo(),
+            'firstname' => $member->getFirstName(),
+            'job' => [
+                'id' => $member->getJob()->getId(),
+                'name' => $member->getJob()->getName()
+                ],
+            'role' => $member->getRoles(),
+            'email' => $member->getEmail(),
+        ];
+    }
+    return new JsonResponse($data);
+    }
+
     //----------------------------------------------MENTORS-----------------------------------------------------------//
     #[Route('/mentors', methods: ['GET'])]
     public function getMentors(MemberRepository $memberRepository): JsonResponse
