b038e29b43d16adafe507da169cddd03ad541436
mathieu <mathieu.perin.prog@gmail.com>
2023-07-04 16:33:50 +0200

    recuperation du token

--- Projet/LeMentorat/src/app/page/login/login.component.ts
+++ Projet/LeMentorat/src/app/page/login/login.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {tap} from "rxjs";
+import {CookieService} from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,7 @@ export class LoginComponent
   form: FormGroup;
   apiUrl = 'http://localhost:8000/login';
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient)
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private cookieService: CookieService)
   {
     this.form = this.formBuilder.group({
       login: ['', Validators.required],
@@ -28,11 +29,13 @@ export class LoginComponent
     {
       const formData = this.form.value;
 
-      this.http.post(this.apiUrl, formData)
+      this.http.post<any>(this.apiUrl, formData)
         .pipe(
           tap(response =>
           {
+            const token = response.token;
             console.log(response);
+            this.cookieService.set('jwt_token', token);
             this.form.reset();
           })
         )
--- Projet/REST_le-Mentorat/src/Controller/Api/LoginController.php
+++ Projet/REST_le-Mentorat/src/Controller/Api/LoginController.php
@@ -2,36 +2,54 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Member;
 use App\Repository\MemberRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class LoginController extends AbstractController
 {
 
-
     #[Route('/login', methods: ['POST'])]
 
-    public function login(Request $request, UserPasswordHasherInterface $hasher, MemberRepository $memberRepository): Response
+    public function login(Request $request,UserPasswordHasherInterface $hasher, MemberRepository $memberRepository, JWTTokenManagerInterface $JWTManager): Response
     {
         $formData = json_decode($request->getContent());
 
         $user = $memberRepository->findByEmailOrPseudo($formData->login);
 
+
         if ($user === null) {
             $response = [
                 'message' => 'Utilisateur non trouvé.',
-
             ];
         } else {
             if ($hasher->isPasswordValid($user, $formData->password)) {
+                $token = $JWTManager->create($user);
+                $admin[] = [
+                    'lastname' => $user->getLastName(),
+                    'description' => $user->getDescription(),
+                    'avatar' => $user->getAvatar(),
+                    'pseudo' => $user->getPseudo(),
+                    'firstname' => $user->getFirstName(),
+                    'job' => [
+                        'id' => $user->getJob()->getId(),
+                        'name' => $user->getJob()->getName()
+                    ],
+                    'role' => $user->getRoles(),
+                    'email' => $user->getEmail(),
+                ];
                 $response = [
                     'message' => 'Connexion réussie.',
-                    'admin' => $user,
+                    'admin' => $admin,
+                    'token' => $token
                 ];
             } else {
                 $response = [
